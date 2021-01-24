049664d454504185da9ee5ba4c452832ccaeb8f6
vivek <vivek.koyani@gmail.com>
2021-01-24 22:44:54 +0530

    add todos component inside functionality

--- src/components/todos.jsx
+++ src/components/todos.jsx
@@ -25,6 +25,52 @@ export default class todos extends Component {
       ],
     };
   }
+
+  //Local helper method to get date
+
+  getTime = () => {
+    let d = new Date();
+    var n = d.getTime();
+    return n;
+  };
+
+  //method called from Todo component
+
+  handleDelete = (todo) => {
+    const todo = this.state.todos.filter((t) => {
+      return t.id !== todo;
+    });
+
+    this.setState({ todos });
+  };
+
+  handleDone = (todo) => {
+    const todos = [...this.state.todos];
+    todos.map((t) => {
+      if (t.id === todo.id) {
+        t.isDone = !t.isDone;
+      }
+      return t;
+    });
+    this.setState({ todos });
+  };
+
+  //method called from AddTodo component
+
+  addNewTodo = (value) => {
+    if (value) {
+      const todos = [...this.state.todos];
+      todos.push({
+        id: this.getTime(),
+        value: value,
+        isDone: false,
+      });
+
+      this.setState({ addTodoValue: "", todos });
+    } else {
+      console.log("Please Add Todo Text");
+    }
+  };
   render() {
     return (
       <table className="table">
@@ -41,7 +87,10 @@ export default class todos extends Component {
           ))}
           <tr>
             <td colSpan="4" className="text-center">
-              <Addtodo fooAddTodo={this.addNewTodo} addTodoValue={this.state.addTodoValue} />
+              <Addtodo
+                fooAddTodo={this.addNewTodo}
+                addTodoValue={this.state.addTodoValue}
+              />
             </td>
           </tr>
         </tbody>
